3dfe85d552a9fae1d565fff16309f1febdf0e309
KANU <kanu@getkanu.com>
2026-01-20 19:26:18 +0000

    feat: Create Terraform ecr.tf with ECR repository and lifecycle policy (50a4c4)

---
+++ terraform/ecr.tf
@@ -0,0 +1,35 @@
+# ECR Repository for backend Docker image
+
+resource "aws_ecr_repository" "backend" {
+  name                 = "${var.project_name}-${var.environment}-backend"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-backend"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "backend" {
+  repository = aws_ecr_repository.backend.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 5 images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 5
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
