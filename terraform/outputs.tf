5b4d5d722b708da01f7434458d573c2f667e06de
KANU <kanu@getkanu.com>
2026-01-20 20:50:16 +0000

    feat: Create Terraform Lambda function and IAM roles for backend (53396e)

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,13 @@ output "cognito_client_id" {
   description = "Cognito User Pool Client ID"
   value       = aws_cognito_user_pool_client.main.id
 }
+
+output "lambda_function_name" {
+  description = "Lambda function name"
+  value       = aws_lambda_function.api.function_name
+}
+
+output "lambda_function_arn" {
+  description = "Lambda function ARN"
+  value       = aws_lambda_function.api.arn
+}
